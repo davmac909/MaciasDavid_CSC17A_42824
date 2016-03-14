a333340979849766258d48ec7ed3f459c3a1b88c
davmac909 <davmac909@gmail.com>
2016-03-14 14:05:39 -0700

    Signed-off-by: davmac909 <davmac909@gmail.com>

--- Class/Dynamic_Array_markSort/main.cpp
+++ Class/Dynamic_Array_markSort/main.cpp
@@ -11,30 +11,33 @@ using namespace std;
 
 //Functional Prototypes
 int *fllArry(int);
-void prntAry(int *, int, int);
-void markSrt(int *, int);
+int *fllIndx(int);
+void prntAry(int *, int *, int, int);
+void markSrt(int *, int *, int);
 int *fllMod(int, int);
 int *mode(int *, int);
 void prntMod(int *, int);
 
 //Execution Begins Here
-int main(int argc, short** argv) {
+int main(int argc, char** argv) {
     //Seed the random Number Generator
     srand(static_cast<int>(time(0)));
     
     //Declare and initialize variables
     int size = 100;
     int *array = fllArry(size);
-    int *modeAry = fllMod(size, 10);
+    int *index = fllIndx(size);
+    
+    //int *modeAry = fllMod(size, 10);
     
     //Output the results
-    prntAry(array, size, 10);
+    prntAry(array, index, size, 10);
     
     //Sort Array
-    markSrt(array, size);
+    markSrt(array, index, size);
     
     //Output the results
-    prntAry(array, size, 10);
+    prntAry(array, index, size, 10);
     
     //Deallocate Memory
     delete []array;
@@ -54,6 +57,21 @@ int *fllArry(int n){
     int *a = new int[n];
     for(int i = 0; i < n; i++){
         *(a+i) = rand()%90+10;
+    }
+    return a;
+}
+/******************************************************************************/
+/*****************************Fill Index***************************************/
+/******************************************************************************/
+//Inputs:
+//  n-> size of array
+//Outputs:
+//  a-> array with random numbers
+int *fllIndx(int n){
+    //Declare and allocate memory
+    int *a = new int[n];
+    for(int i = 0; i < n; i++){
+        *(a+i) = i;
     }
     return a;
 }
@@ -81,11 +99,11 @@ int *fllMod(int n, int mod){
 //  a-> array with random numbers
 //  n-> size of array
 //  perLine-> number of numbers to display per row
-void prntAry(int *a, int n, int perLine){
+void prntAry(int *a, int *b, int n, int perLine){
     //Output the Array
     cout <<endl;
     for(int i = 0; i < n; i++){
-        cout <<a[i] <<" ";
+        cout <<a[b[i]] <<" ";
         if(i%perLine == (perLine-1)){
             cout <<endl;
         }
@@ -99,14 +117,14 @@ void prntAry(int *a, int n, int perLine){
 //  a-> array with random numbers
 //  n-> size of array
 //  perLine-> number of numbers to display per row
-void markSrt(int *a, int n){
+void markSrt(int *a, int *indx, int n){
     for(int pos = 0; pos < n-1; pos++){
         for(int row = pos+1; row < n; row++){
-            if(a[pos] > a[row]){
+            if(a[indx[pos]] > a[indx[row]]){
                 int temp;
-                temp = a[pos];
-                a[pos] = a[row];
-                a[row] = temp;
+                temp = indx[pos];
+                indx[pos] = indx[row];
+                indx[row] = temp;
             }
         }
     }
